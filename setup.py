b10c02833611be0bd805bcd87c236e17e4f462db
gifale95 <alessandro.gifford@gmail.com>
2024-05-18 16:00:41 -0400

    Add setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+requires = []
+with open('requirements.txt') as reqfile:
+    requires = reqfile.read().splitlines()
+
+with open('README.md', encoding='utf-8') as readmefile:
+    long_description = readmefile.read()
+
+
+setup(
+    name='NED',
+    version='0.0.1',
+    description='Neural Encoding Dataset',
+    url='https://github.com/gifale95/NED',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+      "Programming Language :: Python",
+      "Intended Audience :: Science/Research",
+      ],
+    maintainer='Alessandro Gifford',
+    maintainer_email='alessandro.gifford@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires
+)
